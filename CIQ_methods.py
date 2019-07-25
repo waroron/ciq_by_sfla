b4db67975570d881156e7a01810f46d222e803d1
AnominP <ex.gonw122@gmail.com>
2019-07-25 13:14:38 +0900

    Shuffled-Frogs-Leaping Algorithmの実装及び，OneMax問題に対して確認済み. 関数最適化問題や，その最適化過程の可視化，CIQへの適用． 他のアルゴリズムの実装

--- CIQ_methods.py
+++ CIQ_methods.py
@@ -1016,9 +1016,9 @@ def SLFA(img, K):
             shuffled_frogs[mem_worst_index + bd] = next_x_worst
 
 
-def SFLA(fit, create_frog, n_frogs=100, n_mem=10, T_max=100, J_max=100, rho=0.5):
+def SFLA(fit, create_frog, n_frogs=20, n_mem=5, T_max=100, J_max=5, rho=0.5):
     perm = np.arange(0, n_frogs, 1)
-    frogs = np.array([create_frog() for _ in range(n_frogs)])
+    init_frogs = np.array([create_frog() for _ in range(n_frogs)])
 
     def eval_frogs(i_frogs):
         score = np.zeros((len(i_frogs)))
@@ -1027,12 +1027,12 @@ def SFLA(fit, create_frog, n_frogs=100, n_mem=10, T_max=100, J_max=100, rho=0.5)
 
         return score
 
+    shuffled_frogs = init_frogs.copy()
     for t in range(T_max):
-        fitness = eval_frogs(frogs)
         shuffled_index = np.random.permutation(perm)
-        shuffled_frogs = frogs[shuffled_index]
-        shuffled_fitness = fitness[shuffled_index]
-        global_best_index = np.argmax(fitness)
+        shuffled_frogs = shuffled_frogs[shuffled_index]
+        shuffled_fitness = eval_frogs(shuffled_frogs)
+        global_best_index = np.argmax(shuffled_fitness)
 
         print('best frog: {} \n best score: {}'.format(shuffled_frogs[global_best_index],
                                                        shuffled_fitness[global_best_index]))
@@ -1041,24 +1041,26 @@ def SFLA(fit, create_frog, n_frogs=100, n_mem=10, T_max=100, J_max=100, rho=0.5)
         assert n_frogs % n_mem == 0, "wrong setting of num_frogs or num_memeplex"
 
         for bd in range(n_mem, n_frogs, n_mem):
-            index_bius = bd - n_mem
-            mem_best_index = np.argmax(shuffled_fitness[index_bius: bd])
-            mem_worst_index = np.argmin(shuffled_fitness[index_bius: bd])
-            # compute Eqs.(1) and (2)
-            D = rho * (shuffled_fitness[mem_best_index + index_bius] - shuffled_fitness[mem_worst_index + index_bius])
-            next_x_worst = shuffled_frogs[mem_worst_index + index_bius] + D
-            next_x_worst_fit = fit(next_x_worst)
-
-            if next_x_worst_fit < shuffled_fitness[mem_worst_index + index_bius]:
-                # apply Eqs.(1) and (3)
-                D = rho * (shuffled_frogs[-1] - shuffled_frogs[mem_worst_index + bd])
-                next_x_worst = shuffled_frogs[mem_worst_index + bd] + D
+            for j in range(J_max):
+                index_bius = bd - n_mem
+                mem_best_index = np.argmax(shuffled_fitness[index_bius: bd])
+                mem_worst_index = np.argmin(shuffled_fitness[index_bius: bd])
+                # compute Eqs.(1) and (2)
+                D = rho * (shuffled_frogs[mem_best_index + index_bius] - shuffled_frogs[mem_worst_index + index_bius])
+                next_x_worst = shuffled_frogs[mem_worst_index + index_bius] + D
                 next_x_worst_fit = fit(next_x_worst)
 
-                if next_x_worst_fit < shuffled_fitness[mem_worst_index + bd]:
-                    # Move the worst frog to a random position
-                    next_x_worst = create_frog()
-            shuffled_frogs[mem_worst_index + bd] = next_x_worst
+                if next_x_worst_fit < shuffled_fitness[mem_worst_index + index_bius]:
+                    # apply Eqs.(1) and (3)
+                    D = rho * (shuffled_frogs[global_best_index] - shuffled_frogs[mem_worst_index + bd])
+                    next_x_worst = shuffled_frogs[mem_worst_index + bd] + D
+                    next_x_worst_fit = fit(next_x_worst)
+
+                    if next_x_worst_fit < shuffled_fitness[mem_worst_index + index_bius]:
+                        # Move the worst frog to a random position
+                        next_x_worst = create_frog()
+                shuffled_frogs[mem_worst_index + index_bius] = next_x_worst
+                shuffled_fitness[mem_worst_index + index_bius] = next_x_worst_fit
 
 
 def CIQ_from_ColorPalettes(img, palette):
@@ -1396,12 +1398,16 @@ def evaluate_CIQ_bySFLA():
             SLFA(img, c)
 
 
-def MinMaxBySFLA():
+def OneMaxBySFLA():
     def fit(frog):
+        overflow_index = np.where(frog >= 0.5)
+        underflow_index = np.where(frog < 0.5)
+        frog[overflow_index[0]] = 1.0
+        frog[underflow_index[0]] = 0.0
         return np.sum(frog)
 
     def create_frog():
-        return np.random.randint(0, 2, 10)
+        return np.random.randint(0, 2, 10).astype(np.float32)
 
     SFLA(fit, create_frog)
 
@@ -1465,7 +1471,7 @@ if __name__ == '__main__':
     # draw_sugDist()
     # enlarge_and_save()
     # evaluate_CIQ_byIterativeKMeans()
-    MinMaxBySFLA()
+    OneMaxBySFLA()
     # evaluate_CIQ_bySFLA()
     # evaluate_SRandCIQ()
     # evaluate_CIQ_byGA()

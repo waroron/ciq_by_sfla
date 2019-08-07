f8739cd60cf790d7329105f033e92a183cc5f771
Anomino <ex.gonw122@gmail.com>
2019-08-07 16:00:57 +0900

    BTPDの動作確認

--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,7 @@
 import numpy as np
+import os
+import cv2
+import time
 
 
 def SFLA(fit, create_frog, n_frogs=20, n_mem=5, T_max=100, J_max=5, rho=0.5):
@@ -54,6 +57,68 @@ def SFLA(fit, create_frog, n_frogs=20, n_mem=5, T_max=100, J_max=5, rho=0.5):
     return shuffled_frogs[global_best_index], hist_bestfrogs, hist_frogs
 
 
+def BTPD(S, M):
+    C = []
+    R = []
+    m = []
+    N = []
+    q = []
+
+    def get_R(c):
+        sum = (c[0] * c[0].T).copy()
+        for s in c[1:]:
+            tmp = s * s.T
+            sum += tmp
+        return sum
+
+    def get_m(c):
+        sum = c[0].copy()
+        for s in c[1:]:
+            sum += s
+        return sum
+
+    def get_N(c):
+        return len(c)
+
+
+    C.append(S)
+    R.append(get_R(C[0]))
+    m.append(get_m(C[0]))
+    N.append(get_N(C[0]))
+    q.append(m[0] / N[0])
+
+    for num in range(M - 1):
+        R_ = R[num] - (m[num] * m[num].T) / N[num]
+        W, v = np.linalg.eig(R_)
+        e = v[np.argmax(W)]
+
+        criteria = np.dot(e, q[num][0])
+        compare = np.dot(e, C[num][:, 0, :].T)
+        c_2n_index = np.where(compare <= criteria)
+        c_2n1_index = np.where(compare > criteria)
+        num_c2n = len(c_2n_index[0])
+        num_c2n1 = len(c_2n1_index[0])
+
+        c_2n = np.reshape(C[num][c_2n_index[0]], (num_c2n, 1, 3))
+        c_2n1 = np.reshape(C[num][c_2n1_index[0]], (num_c2n1, 1, 3))
+
+        C.append(c_2n)
+        C.append(c_2n1)
+
+        R.append(get_R(c_2n))
+        m.append(get_m(c_2n))
+        N.append(get_N(c_2n))
+        q.append(m[-1] / N[-1])
+
+        R.append(R[num] - R[-1])
+        m.append(m[num] - m[-1])
+        N.append(N[num] - N[-1])
+        q.append(m[-1] / N[-1])
+
+    color_palette = np.round(q[len(q) - M:])
+    return color_palette
+
+
 def OneMaxBySFLA():
     def fit(frog):
         overflow_index = np.where(frog >= 0.5)
@@ -68,5 +133,31 @@ def OneMaxBySFLA():
     SFLA(fit, create_frog)
 
 
+def BTPD_CIQ(img, M):
+    S = np.reshape(img, (img.shape[0] * img.shape[1], 1, 3)).astype(np.uint64)
+    color_palette = BTPD(S, M)
+
+    return color_palette
+
+
+def CIQ_test_BTPD():
+    DIR = 'sumple_img'
+    M = 16
+    imgs = os.listdir(DIR)
+
+    for img_path in imgs:
+        path = os.path.join(DIR, img_path)
+        img = cv2.imread(path)
+        st = time.time()
+        q = BTPD_CIQ(img, M)
+        en = time.time()
+        print('{}: {} colors pallete by BTPD: time {}'.format(img_path, M, en - st))
+        for pix in q:
+            print(pix)
+
+        print('\n\n')
+
+
 if __name__ == '__main__':
-    OneMaxBySFLA()
+    CIQ_test_BTPD()
+    # OneMaxBySFLA()
